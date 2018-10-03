622fe0654ed79b53b169fdc30053389470460863
Yousof Mersal <yousof777@gmail.com>
2018-10-03 23:35:51 +0200

    Add a landing page route

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-const app = require('express')
-const bodyParse = require('body-parse')
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const app = express()
+const port = 9001
+
+app.get('/', (req, res)=>{
+    fs.readFile('./views/landing.html', 'utf-8', (error, data)=>{
+        if (error) {
+            try {
+                res.sendStatus(500).send('Oops! Something Went wrong!')
+            } catch (error) {
+                console.log(error)
+            }
+        }else {
+            res.write(data)
+            res.end()
+        }
+    })
+})
+
+app.listen(port, ()=>{
+    console.log('Koala Keeper serving to the world!')
+})
