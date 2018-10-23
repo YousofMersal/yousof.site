f87a6bc24f78e2be300cf434d2dfe08df1c96b3d
Yousof Mersal <yousof777@gmail.com>
2018-10-23 20:42:35 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 9001, ()=>{
+    console.log('Tets is up')
+});
