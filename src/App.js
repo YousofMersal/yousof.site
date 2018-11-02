4eef45f0ea415c2d242e8a65b15ce0373ce459cf
Yousof Mersal <yousof777@gmail.com>
2018-11-03 00:17:00 +0100

    add logo and remove some scaffolding

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MovieCard from './components/moviecard'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            test
-          </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Placeholder title</h1>
+                <MovieCard />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
