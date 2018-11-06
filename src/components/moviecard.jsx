7f4ce4c061f4f7f07f92c9ed89923105cd97908d
Yousof Mersal <yousof777@gmail.com>
2018-11-06 20:48:05 +0100

    add no poster image for movies without posters

--- src/components/moviecard.jsx
+++ src/components/moviecard.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class MovieCard extends Component {
+    checkIfImage(img) {
+        if (img === 'N/A') {
+            return require('./assets/NoPoster.jpg')
+            // return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa9Ay_5cDRi_Xm1HEdtTVH1m3JUixM6lW3c_f0-3cWGss7Ua8s'
+        } else {
+            return img
+        }
+    }
     render() {
         return (
             <div className="moviecard">
                 <li>
                     <h2>{this.props.name}</h2>
-                    <img src={this.props.img} alt="None available" />
+                    <img
+                        src={this.checkIfImage(this.props.img)}
+                        alt="None available"
+                    />
                 </li>
             </div>
         )
