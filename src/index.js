c7736ee1c92e7a5c730004ed5b2cde6d742ecaee
Yousof Mersal <yousof777@gmail.com>
2018-11-13 15:09:24 +0100

    add better file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import App from './containers/App.jsx'
 import './style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
