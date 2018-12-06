ecbb53827a73e3844667ca85c6ed9d8c755c835a
Yousof Mersal <yousof777@gmail.com>
2018-12-06 16:35:22 +0100

    set port to variable to be able to edit later

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const buildPath = path.join(__dirname, '../../build')
+const port = process.env.PORT || 9001
 app.use(express.static(buildPath))
 
 app.get('/*', function(req, res) {
@@ -9,6 +10,6 @@ app.get('/*', function(req, res) {
   res.sendFile(indexPath)
 })
 
-app.listen(process.env.PORT || 9001, () => {
+app.listen(port, () => {
   console.log('Koala Keeper is up and running')
 })
