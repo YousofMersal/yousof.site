81c58424cd5f6e747fa255120415e782ee5032b1
Yousof Mersal <yousof777@gmail.com>
2019-01-06 11:22:19 +0100

    add render for movie data from request

--- src/client/components/MovieCardList.jsx
+++ src/client/components/MovieCardList.jsx
@@ -4,16 +4,16 @@ import MovieCard from './MovieCard'
 export default class MovieCardList extends Component {
   render() {
     if (this.props.data === undefined || this.props.data.length < 1) {
-      return <h2 className="nomovies"> You need to search for a movie </h2>
+      return <h2 className='nomovies'> You need to search for a movie </h2>
     } else {
       return (
-        <div className="moviecardlist">
+        <div className='moviecardlist'>
           {this.props.data.map(item => (
             <MovieCard
-              img={item.Poster}
-              name={item.Title}
-              key={item.imdbID}
-              id={item.imdbID}
+              img={`https://image.tmdb.org/t/p/w342//${item.poster_path}`}
+              name={item.original_title}
+              key={item.id}
+              id={item.id}
             />
           ))}
         </div>
