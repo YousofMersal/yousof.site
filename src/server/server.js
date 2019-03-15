19255bf76f29441928e6646e3fbb76118c9f355b
Yousof Mersal <yousof777@gmail.com>
2019-03-15 17:16:10 +0100

    start on sessions

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
-// const mongoose = require('mongoose')
-const passportsetup = require('./auth/passport-setup')
+require('./auth/passport-setup')
 const passport = require('passport')
 const Axios = require('axios')
 const apikey = process.env.APIKEY
 const express = require('express')
 const path = require('path')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const app = express()
 const buildPath = path.join(__dirname, '../../build')
@@ -15,24 +15,30 @@ var CronJob = require('cron').CronJob
 app.use('/user', authrouter.router)
 app.use(bodyParser.json())
 app.use(express.static(buildPath))
+const sessionOptions = {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  secret: process.env.ENVIROMENT === 'prod' ? process.env.SECRET : false
+}
+console.log(sessionOptions)
+app.use(session(sessionOptions))
+
 app.use(passport.initialize())
 app.use(passport.session())
 
-let movieConfig = Axios.get(
-  'https://api.themoviedb.org/3/configuration?api_key=d65f7650048ab646ecf08931d26d9be4'
-)
-const job = new CronJob('*/4 * * * * *', () => {
-  Axios.get(
-    'https://api.themoviedb.org/3/configuration?api_key=d65f7650048ab646ecf08931d26d9be4'
-  )
-    .then(res => (movieConfig = res))
-    .catch(err => console.log(err))
-})
-job.start()
-// mongoose.connect(
-//   process.env.MONGODB_URI,
-//   { useNewUrlParser: true }
+// let movieConfig = Axios.get(
+//   'https://api.themoviedb.org/3/configuration?api_key=d65f7650048ab646ecf08931d26d9be4'
 // )
+
+// const job = new CronJob('*/4 * * * * *', () => {
+//   Axios.get(
+//     'https://api.themoviedb.org/3/configuration?api_key=d65f7650048ab646ecf08931d26d9be4'
+//   )
+//     .then(res => {
+//       movieConfig = res
+//     })
+//     .catch(err => console.log(err))
+// })
+// job.start()
 
 app.use('/auth', authrouter.router)
 
@@ -48,6 +54,7 @@ app.post('/api/getmultiple', (req, res) => {
 })
 
 app.post('/api/getsingle', (req, res) => {
+  console.log(req.session.cookie)
   const id = req.body.data
   Axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}&language=en-US`
