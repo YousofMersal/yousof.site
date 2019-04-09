4cfe1fc4bbb777e5c033ace4ae9652e0921eecb6
Yousof Mersal <yousof777@gmail.com>
2019-04-09 17:08:48 +0200

    Profile logout redirects but don'set update state

--- src/client/components/user/Profile.jsx
+++ src/client/components/user/Profile.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import { logOut } from '../../api/UserAPI'
-import { Redirect } from 'react-router-dom'
-
-/* TODO
- * Fix redirect issue after logout attempt
- */
 
 const Profile = props => {
   const handleLogOut = () => {
-    logOut().then(res => (res.data === 'OK' ? <Redirect to='/' /> : null))
+    logOut().then(res => (res.data === 'OK' ? props.history.push('/') : null))
   }
   return <button onClick={handleLogOut}>logout</button>
 }
