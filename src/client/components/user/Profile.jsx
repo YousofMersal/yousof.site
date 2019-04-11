cc08499b192cbbede8d0c0ef0822429600d02d2e
Yousof Mersal <yousof777@gmail.com>
2019-04-11 23:02:16 +0200

    Connect profile.jsx to state and fire sessionupdate when logout is clicked

--- src/client/components/user/Profile.jsx
+++ src/client/components/user/Profile.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import actionTypes from '../../store/actions/actions'
 import { logOut } from '../../api/UserAPI'
 
 const Profile = props => {
   const handleLogOut = () => {
-    logOut().then(res => (res.data === 'OK' ? props.history.push('/') : null))
+    logOut().then(res =>
+      res.data === 'OK' ? props.onSessionStatus() && props.history.push('/') : null
+    )
   }
   return <button onClick={handleLogOut}>logout</button>
 }
 
-export default Profile
+const mapStateToProps = (state, ownProps) => {
+  return {
+    isLoggedIn: state.isLoggedIn
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSessionStatus: () => dispatch(actionTypes.sessionStatus())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile)
