226980cc6542ebc3f1620b7135c257acbf6cb6f8
Yousof Mersal <yousof777@gmail.com>
2019-04-12 17:14:17 +0200

    log cleanup

--- src/client/components/user/form/LogoutButton.jsx
+++ src/client/components/user/form/LogoutButton.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { logOut } from '../../../api/UserAPI'
 
 const LogoutButton = props => {
-  console.log(props)
   const handleLogOut = () => {
     logOut().then(res =>
       res.data === 'OK' ? props.onSessionStatus() && props.history.push('/') : null
